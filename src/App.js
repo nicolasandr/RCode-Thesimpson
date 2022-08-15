e7e6c0883f21adfefba4ed6f91a459f87498ccd8
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-15 16:12:24 -0300

    cargamos imagen,frase y nombre de cada personaje de los psimpson a traves de una API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import Frase from "./components/Frase";
 import "bootstrap/dist/css/bootstrap.min.css";
 import thesimpsons from "../src/img/theSimpson.png";
 import "../src/App.css";
 import { Button } from "react-bootstrap";
+
 const App = () => {
+  
+    const [personaje,setPersonaje]= useState({});
+
+    useEffect(()=>{
+      consultarAPI();
+    },[])
+
+    const consultarAPI = async()=> {
+      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const dato =await respuesta.json();
+     console.log(dato[0]);
+     setPersonaje(dato[0]);
+    }
+  
   return (
     <div className="bg-fondo-web">
       <div className="container py-5 text-center">
@@ -16,12 +31,12 @@ const App = () => {
       </div>
 
       <div className="text-center pb-10">
-        <Button className="btn btn-color">
+        <Button className="btn btn-color" onClick={consultarAPI}>
           <p className="my-0 text-btn-style">Obtener frase</p>
         </Button>
       </div>
 
-      <Frase></Frase>
+      <Frase personaje={personaje}></Frase>
     </div>
   );
 };
